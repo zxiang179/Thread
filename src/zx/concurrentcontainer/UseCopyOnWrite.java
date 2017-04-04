cadedc6507653e83de0f5e05448a0dc85de646e6
zx <zxiang248@gmail.com>
2017-04-04 13:03:33 +0800

    并发容器的使用ConcurrentHashMap/CopyOnWrite

---
+++ src/zx/concurrentcontainer/UseCopyOnWrite.java
@@ -0,0 +1,13 @@
+package zx.concurrentcontainer;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class UseCopyOnWrite {
+	
+	CopyOnWriteArrayList<String> cwal = new CopyOnWriteArrayList<String>();
+	CopyOnWriteArraySet<String> cwas = new CopyOnWriteArraySet<String>();
+	
+	
+
+}
